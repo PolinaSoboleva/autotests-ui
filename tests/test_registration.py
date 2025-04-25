d360c0d8dc83081a1a276d0fc22ab6b360ffebff
PolinaSoboleva <lokpolinaa@gmail.com>
2025-04-25 21:16:36 +0300

    Интеграция Pytest и Playwright

---
+++ tests/test_registration.py
@@ -0,0 +1,32 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_successful_registration():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        contex = browser.new_context()  # чтобы сохрнять данные в локал сторедж
+        page = contex.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('user.name@gmail.com')
+
+        name_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        name_input.fill('username')
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('password')
+
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        contex.storage_state(path='browser-state.json')
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        contex = browser.new_context(storage_state='browser-state.json')  # чтобы сохрнять данные в локал сторедж
+        page = contex.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
+        page.wait_for_timeout(5000)
